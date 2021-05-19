a322ddbff3b44c41e1813e186e3923aa3964c2cd
srigopinadh <srigopinadh@LAPTOP-FI3VUDAV>
2021-05-19 22:30:01 +0530

    First Commit-primusbank test

--- Primusabank_TestngFramework/src/utils/Apputils.java
+++ Primusabank_TestngFramework/src/utils/Apputils.java
@@ -5,9 +5,9 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
+
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
+
 
 public class Apputils
 {
